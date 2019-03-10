ca61b2f8fe3adc0e48b9c9bb1d4bb6c447924d16
Le Oanh <leoanh@H.Oanh>
2019-03-10 18:08:09 +0700

    basic+catchermove

---
+++ game.js
@@ -0,0 +1,90 @@
+let canvas; 
+let context; 
+
+canvas=document.createElement("canvas"); 
+context = canvas.getContext("2d"); 
+canvas.width = 512; 
+canvas.height = 480; 
+
+document.body.appendChild(canvas); 
+
+let startTime = Date.now(); 
+let elapsedTime; 
+let roundTime= 30; 
+
+let bgImage; 
+let oppImage; 
+let catcherImage; 
+
+let bgReady; 
+let oppReady; 
+let catcherReady; 
+
+function loadImages()
+{ bgImage = new Image(); 
+  bgImage.onload = function(){ bgReady=true; };
+  bgImage.source= "image/catchopps2bg.jpeg";
+  oppImage = new Image(); 
+  oppImage.onload = function(){ oppReady=true; };
+  bgImage.source= "image/opps1.png"; 
+  catcherImage= new Image();
+  catcherImage.onload =function(){catcherReady=true;};
+  catcherImage.source="image/catcher2.jpg"
+}
+let oppX=100;
+let oppY=100;
+let catcherX= canvas.width/2;
+let catcherY= canvas.height/2; 
+let keyDown={}; 
+
+function setupKeyboardListeners()
+{ addEventListener("keydown", function(key){ keyDown[key.keyCode]=true;  }, false);
+ addEventListener("keyup", function(key){ delete keyDown[key.keyCode]},false );
+}
+let update = function ()
+{elapsedTime= Math.floor((Date.now() - startTime)/1000);  
+
+if (38 in keyDown){ catcherY -= 5; }
+if (40 in keyDown){ catcherY += 5; }
+if (37 in keyDown){ catcherX -= 5; }
+if (39 in keyDown){ catcherX += 5; }
+
+if (
+    catcherX <= (oppX + 32)
+    && oppX <= (catcherX + 32)
+    && catcherY <= (oppY + 32)
+    && oppY <= (catcherY + 32)
+  ) {
+    // Pick a new location for the monster.
+    // Note: Change this to place the monster at a new, random location.
+    oppX = oppX + 50;
+    oppY = oppY + 70;
+  }
+};
+let render = function()
+{
+    if (bgReady ) {context.drawImage(bgImage,0,0); }
+    if (oppReady) {context.drawImage(oppImage,oppX,oppY); }
+    if (catcherReady) { context.drawImage(catcherImage, catcherX, catcherY); }
+    context.fillText (`Remaining time: ${roundTime-elapsedTime}`,20,100)
+ }
+let main = function()
+{ update();  render(); requestAnimationFrame(main); 
+}
+let w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+loadImages();
+setupKeyboardListeners(); 
+main(); 
+
+
+
+
+
+
+
+
+
+    
+
