77129d5bc15d83d91115da5a2c3e475b50021ee3
Le Oanh <leoanh@H.Oanh>
2019-03-11 08:58:19 +0700

    update for review the boundery

--- game.js
+++ game.js
@@ -19,6 +19,7 @@ let catcherImage;
 let bgReady; 
 let oppReady; 
 let catcherReady; 
+let oppNum=0; 
 
 function loadImages()
 { bgImage = new Image(); 
@@ -26,10 +27,10 @@ function loadImages()
   bgImage.src= "images/catchoppsbg.jpeg";
   oppImage = new Image(); 
   oppImage.onload = function(){ oppReady=true; };
-  bgImage.src= "images/opps2.png"; 
+  oppImage.src= "images/opps2.png"; 
   catcherImage= new Image();
   catcherImage.onload =function(){catcherReady=true;};
-  catcherImage.src="images/catcher4.png"
+  catcherImage.src="images/catcher4.png"; 
 }
 let oppX=100;
 let oppY=100;
@@ -44,33 +45,48 @@ function setupKeyboardListeners()
 let update = function ()
 {elapsedTime= Math.floor((Date.now() - startTime)/1000);  
 
-if (38 in keyDown){ catcherY -= 5; }
-if (40 in keyDown){ catcherY += 5; }
-if (37 in keyDown){ catcherX -= 5; }
-if (39 in keyDown){ catcherX += 5; }
+if (38 in keyDown){if (catcherY > 0 && catcherY < canvas.height) {catcherY -= 5;}
+else {catcherY +=2; }}
+
+if (40 in keyDown){if (catcherY > 0 && catcherY < canvas.height ) {catcherY += 5;} 
+else {catcherY -=2; }} 
+
+if (37 in keyDown){if (catcherX >= 0 && catcherY <= canvas.width  ) {catcherX -= 5;}}
+if (39 in keyDown){if (catcherX >= 0 && catcherX <= canvas.width  ) {catcherX += 5;}}
 
 if (
-    catcherX <= (oppX + 32)
-    && oppX <= (catcherX + 32)
-    && catcherY <= (oppY + 32)
-    && oppY <= (catcherY + 32)
-  ) {
+    catcherX <= (oppX + 50)
+    && oppX <= (catcherX + 50)
+    && catcherY <= (oppY + 50)
+    && oppY <= (catcherY + 50)
+  )
+  { 
+    oppNum++; 
+    oppImage.src= "images/opp5.png"; 
     // Pick a new location for the monster.
     // Note: Change this to place the monster at a new, random location.
-    oppX = oppX + 50;
-    oppY = oppY + 70;
+    oppX = Math.floor(Math.random()*550);
+    oppY = Math.floor(Math.random()*450);
   }
 };
 let render = function()
-{
+{    let timeleft =roundTime-elapsedTime; 
+    
     if (bgReady ) {context.drawImage(bgImage,0,0); }
     if (oppReady) {context.drawImage(oppImage,oppX,oppY); }
     if (catcherReady) { context.drawImage(catcherImage, catcherX, catcherY); }
-    context.fillText (`Remaining time: ${roundTime-elapsedTime}`,20,100)
- }
+    if (timeleft>0)
+    {context.fillText(`Remaining time: ${timeleft}`,20,50);}
+    else {context.fillText(`Game Over`,250,200); deleteAnimationFram (main); }
+    context.fillText(`Caught stars: ${oppNum}`,20,30);
+    //if ( catcherX == canvas.width || catcherY == canvas.height ) 
+    //{ catcherX = canvas.width; catcherY = canvas.height; }
+
+ };
+ 
 let main = function()
 { update();  render(); requestAnimationFrame(main); 
-}
+}; 
 let w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
